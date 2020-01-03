88252bd76759881c1f909f47b8de67049a0d9bcd
Mehedi Hasan Likhon <mehedi.hasansjs@gmail.com>
2020-01-03 18:46:08 +0600

    hello

--- js/main.js
+++
@@ -1,3 +0,0 @@
-// let today = new Date(`${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()}`).getTime();
-// let deadline = new Date("01/10/2020").getTime();
-// let dayBaki = (deadline - today) / (1000 * 3600 * 24);
